7399af55899d37b6768edc2add75ea50da8dd313
Guilherme Henrique <guizuh_2014@outlook.com>
2026-06-15 22:24:34 -0300

    added biome fix on save

--- src/features/customize/core/customization-options/CustomizationData.coordinator.ts
+++ src/features/customize/core/customization-options/CustomizationData.coordinator.ts
@@ -1,15 +1,18 @@
 import { CombinationTracker } from "./Combination.tracker";
-import { CustomizationCategory, CustomizationData } from "./types/CustomizationData";
+import type {
+  CustomizationCategory,
+  CustomizationData,
+} from "./types/CustomizationData";
 import { buildCombinationString } from "./utils/buildCombinationString";
 
 export class CustomizationDataCoordinator {
-    private combinationTracker: CombinationTracker<CustomizationCategory>;
-    private combinationsStringCache: Record<string, CustomizationData> = {};
+  private combinationTracker: CombinationTracker<CustomizationCategory>;
+  private combinationsStringCache: Record<string, CustomizationData> = {};
 
   constructor() {
     this.combinationTracker = new CombinationTracker<CustomizationCategory>();
   }
-  
+
   public saveData(data: CustomizationData) {
     // this.combinationTracker.addCombination(data);
     const currCombination = this.combinationTracker.getLastCombination();
